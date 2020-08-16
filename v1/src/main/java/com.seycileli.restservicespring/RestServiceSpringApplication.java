5a98617b05eb70d702f5ab5de46b2f95908bfa67
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-16 17:37:55 -0400

    Create RestServiceSpringApplication.java

---
+++ v1/src/main/java/com.seycileli.restservicespring/RestServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.seycileli.restservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceSpringApplication.class, args);
+    }
+
+}
