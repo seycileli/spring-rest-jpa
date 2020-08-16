1070314c998eb8071f7bf1c5309b5ee032be4271
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-16 17:40:17 -0400

    Create CourseRepository.java

---
+++ v1/src/main/java/com.seycileli.restservicespring/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.seycileli.restservicespring.repository;
+
+import com.seycileli.restservicespring.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, String> { }
