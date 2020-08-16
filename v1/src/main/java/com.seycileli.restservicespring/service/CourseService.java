3e1eec00c2c55383ed7ae36de1620fbba69a7499
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-16 17:41:00 -0400

    Create CourseService.java

---
+++ v1/src/main/java/com.seycileli.restservicespring/service/CourseService.java
@@ -0,0 +1,40 @@
+package com.seycileli.restservicespring.service;
+
+import com.seycileli.restservicespring.model.Course;
+import com.seycileli.restservicespring.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+    private CourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> getAllCourses(String topicId) {
+        return courseRepository.findAllById(Collections.singleton(topicId));
+    }
+
+    public Optional<Course> getCourse(String id) {
+        return courseRepository.findById(id);
+    }
+
+    public void addCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse(String id) {
+        courseRepository.deleteById(id);
+    }
+}
